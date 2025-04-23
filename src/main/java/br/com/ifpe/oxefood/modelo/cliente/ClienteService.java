cd71b081749ebb58df33f002c9327fd6d51f0282
Aluno <aluno@ifpe.com>
2025-04-23 20:49:36 -0300

    Implementação da Listagem (Postman)

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,16 @@ public class ClienteService {
        //Cliente c = repository.save(cliente);
        //return c;
    }
+   
+   public List<Cliente> listarTodos() {
+  
+    return repository.findAll();
+}
+
+public Cliente obterPorID(Long id) {
+
+    return repository.findById(id).get();
+}
 
 
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,17 @@ public class EntregadorService {
        return repository.save(entregador);
        
    }
+
+  public List<Entregador> listarTodos() {
+  
+        return repository.findAll();
+    }
+
+    public Entregador obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+
 
 }
