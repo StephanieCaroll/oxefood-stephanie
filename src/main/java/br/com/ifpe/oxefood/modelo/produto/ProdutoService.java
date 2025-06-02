ddefa0f8506b0f52c4c76aa7a1da6558f181d2ae
Stephanie <stephaniecaroline2006@gmail.com>
2025-06-02 01:14:17 -0300

    EnderecoCliente, alteraçoes esteticas

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.ifpe.oxefood.util.entity.exception.ProdutoException;
+import br.com.ifpe.oxefood.util.exception.ProdutoException;
 import jakarta.transaction.Transactional;
 
 @Service
