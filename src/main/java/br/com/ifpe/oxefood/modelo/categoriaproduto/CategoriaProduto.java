06cb3b286b080be92d053c1aa5cb3b88ac15764d
Stephanie <stephaniecaroline2006@gmail.com>
2025-06-15 12:13:03 -0300

    Atualizações

--- src/main/java/br/com/ifpe/oxefood/modelo/categoriaproduto/CategoriaProduto.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/categoriaproduto/CategoriaProduto.java
@@ -1,11 +1,14 @@
 package br.com.ifpe.oxefood.modelo.categoriaproduto;
 
 import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.validator.constraints.Length;
 
 import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +26,10 @@ import lombok.Setter;
 public class CategoriaProduto extends EntidadeAuditavel{
     
 @Column
+
+   @NotNull(message = "O Nome é de preenchimento obrigatório")
+   @NotEmpty(message = "O Nome é de preenchimento obrigatório")
+   @Length(max = 100, message = "O Nome deverá ter no máximo 100 caracteres")
    private String descricao;
 
 }
